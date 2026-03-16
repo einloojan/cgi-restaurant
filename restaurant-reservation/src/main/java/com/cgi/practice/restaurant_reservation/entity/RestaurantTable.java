45cde454c0da435c9872a07bf45ac817283fbc7b
Jan Aaron Einloo <janeinlo@tlu.ee>
2026-03-17 00:06:42 +0200

    First RestaurantTable entity and according enum

---
+++ restaurant-reservation/src/main/java/com/cgi/practice/restaurant_reservation/entity/RestaurantTable.java
@@ -0,0 +1,33 @@
+package com.cgi.practice.restaurant_reservation.entity;
+
+import com.cgi.practice.restaurant_reservation.enums.Zone;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class RestaurantTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tableNumber;
+    private int capacity;
+
+    @Enumerated(EnumType.STRING)
+    private Zone zone;
+
+    private int x;
+    private int y;
+
+    private boolean windowSeat;
+    private boolean quietArea;
+    private boolean accessible;
+    private boolean nearPlayArea;
+}
---
+++ restaurant-reservation/src/main/java/com/cgi/practice/restaurant_reservation/enums/Zone.java
@@ -0,0 +1,7 @@
+package com.cgi.practice.restaurant_reservation.enums;
+
+public enum Zone {
+    INDOOR,
+    TERRACE,
+    PRIVATE_ROOM
+}
