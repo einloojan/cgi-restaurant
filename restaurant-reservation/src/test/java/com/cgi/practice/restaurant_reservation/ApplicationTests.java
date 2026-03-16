ec7088b22b338848a87ad956c4aa39ace60f367a
Jan Aaron Einloo <janeinlo@tlu.ee>
2026-03-16 19:49:30 +0200

    Add Spring Boot backend project

---
+++ restaurant-reservation/src/test/java/com/cgi/practice/restaurant_reservation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.practice.restaurant_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
